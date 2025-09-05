a5018a57c12f32efc2b17479becef535913ae5b5
elisabethroys <125894666+elisabethroys@users.noreply.github.com>
2025-09-05 11:07:35 +0200

    Made all the names in the people array show in the friends list

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-people',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './people.component.css'
 })
 export class PeopleComponent {
-
+  @Input() name: string = '';
 }
