ae30f873b5a4952bf9d9e34ea4dc4f4b24eac5bd
elisabethroys <125894666+elisabethroys@users.noreply.github.com>
2025-09-05 10:49:02 +0200

    Generated the people component, and made the component accessable for app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PeopleComponent } from './people/people.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, PeopleComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
